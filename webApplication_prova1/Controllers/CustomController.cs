db80f8580da12e86b8959c8e7dc67f8c23c6171d
Alessandro Durigon <taitto.dury@hotmail.com>
2022-12-16 17:59:59 +0100

    commit prova1

---
+++ webApplication_prova1/Controllers/CustomController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webApplication_prova1.Controllers
+{
+    public class CustomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CustomPage()
+        {
+            return View();
+        }
+    }
+}
